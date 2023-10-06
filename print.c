8d0f2af2ee684288037329e6f1d90b9bf8b6eef7
Moniaar <www.omniaahmed1562001@gmail.com>
2023-10-06 02:54:06 -0700

    here is the printing function you're asking for

---
+++ print.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * display - a printf function 
+ *
+ * @string: the string to be printed
+ *
+ * @value: the printed to be value
+ *
+ * Return: standard output
+ */
+
+void display(char *string, int value)
+{
+	int k;
+
+	k = 0;
+
+	for (; string[k] != '\0' ; k++)
+	{
+		write(stream, &string[k], 1);
+	}
+}
