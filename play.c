33115b31e967cd1eb9556a16064aa8bede4e528f
SolomonChidera <solochidera10@gmail.com>
2023-10-10 21:03:32 +0100

    (Add) play.c

---
+++ play.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_PATH_LENGTH 1024
+
+// Function to find a command in the PATH
+char *find_command_in_path(const char *command)
+{
+	char *path = getenv("PATH");
+	char *token = strtok(path, ":");
+
+	while (token != NULL)
+	{
+		char full_path[MAX_PATH_LENGTH];
+		write(1, &full_path, strlen(full_path));
+		_printf("%d", MAX_PATH_LENGTH);
+		write(1, &token, strlen(token));
+		write(1, &command, strlen(command));
+
+		if (access(full_path, X_OK) == 0)
+		{
+			// Found the command in the current path entry
+			return strdup_(full_path);
+		}
+
+		token = strtok(NULL, ":");
+	}
+
+	// Command not found in PATH
+	return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <command>\n", argv[0]);
+		return 1;
+	}
+
+	char *command = argv[1];
+	char *path_to_command = find_command_in_path(command);
+
+	if (path_to_command != NULL) {
+		printf("Found %s at: %s\n", command, path_to_command);
+		free(path_to_command);
+	} else {
+		printf("%s not found in PATH\n", command);
+	}
+
+	return 0;
+}
