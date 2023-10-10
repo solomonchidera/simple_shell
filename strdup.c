dd21a6bdf3a88e96a3b9c954fd2a6cb3057880d4
SolomonChidera <solochidera10@gmail.com>
2023-10-10 21:07:49 +0100

    strdup function

---
+++ strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * strdup_ - a function to duplicate string
+ *
+ * @string: the string to duplicating
+ *
+ * Return: pointer to the duplicated string
+ */
+
+char strdup_(const char *string)
+{
+        int len;
+        char *r;
+
+        len = 0;
+
+        if (string == NULL)
+                return (NULL);
+        while (*string++)
+                len++;
+        r = malloc(sizeof(char) * (len + 1));
+        if (!r)
+                return (NULL);
+        for (len++; len-- ; )
+                r[len] = *--string;
+
+        return (string);
+}
