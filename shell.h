7dea7ffe08cadf5ed0373cee91a8a5ec4ab95f5f
SolomonChidera <solochidera10@gmail.com>
2023-10-11 18:51:02 +0100

    fix: Update on Header file

--- shell.h
+++ shell.h
@@ -9,8 +9,47 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <errno.h>
+#include <stdarg.h>
 
 #define MAX_PATH_LENGTH 1024
+
+/**
+ * strcut built - contains a builtin string and some related functions
+ *
+ * @flag: pointer to the built in command flag
+ *
+ * @fu: common function
+ */
+
+typedef struct built built_t;
+
+struct built
+{
+        char *flag;
+        int(*fu)(details*);
+}built_t;
+
+
+/**
+ * struct common_arg - contains arguments to be passed into a function
+ *
+ * @l_errorcount: error counting
+ *
+ * @read_fd: the fd to be read from the input line
+ */
+
+typedef struct common_arg details;
+
+struct common_arg
+{
+        unsigned int l_errorcount;
+        int read_fd;
+        char *fdname;
+        char *arg;
+        char *argv;
+        char *path;
+        int argc;
+}details;
 
 void user_command(char *commands, size_t size);
 char **split(char *string, char *del);
@@ -30,39 +69,5 @@ int _putchar(char c);
 int _strcomp(char *st1, char *st2);
 int _strlen(char *string);
 
-
-/**
- * strcut built - contains a builtin string and some related functions
- *
- * @flag: pointer to the built in command flag
- *
- * @fu: common function
- */
-
-struct built
-{
-        char *flag;
-        int(*fu)(details*);
-}built_t;
-
-
-/**
- * struct common_arg - contains arguments to be passed into a function
- *
- * @l_errorcount: error counting
- *
- * @read_fd: the fd to be read from the input line
- */
-
-struct common_arg
-{
-        unsigned int l_errorcount;
-        int read_fd;
-        char *fdname;
-        char *arg;
-        char *argv;
-        char *path;
-        int argc;
-}details;
 
 #endif
