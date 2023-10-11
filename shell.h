0ba177b965e878e0ba88185e2454a15cf1c0a74b
Moniaar <www.omniaahmed1562001@gmail.com>
2023-10-11 02:50:29 -0700

    debugging sesion 1

--- shell.h
+++ shell.h
@@ -8,10 +8,60 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <errno.h>
+
+#define MAX_PATH_LENGTH 1024
 
 void user_command(char *commands, size_t size);
+char **split(char *string, char *del);
+char strdup_(const char *string);
+int check_del(char ch, char *del);
+char strdup_(const char *string);
+char *find_command_in_path(const char *command);
+char *locateChar(char *string, char c);
+int sloop(details *n, char **av);
+int find_(details *n);
 void _puts(char *str);
+void set_details(details *a, char **av);
 void display_prompt(void);
 void exec_command(const char *commands);
 int _putchar(char c);
+int _strcomp(char *st1, char *st2);
+int _strlen(char *string);
+
+
+/**
+ * strcut built - contains a builtin string and some related functions
+ *
+ * @flag: pointer to the built in command flag
+ *
+ * @fu: common function
+ */
+
+struct built
+{
+        char *flag;
+        int(*fu)(details*);
+}built_t;
+
+
+/**
+ * struct common_arg - contains arguments to be passed into a function
+ *
+ * @l_errorcount: error counting
+ *
+ * @read_fd: the fd to be read from the input line
+ */
+
+struct common_arg
+{
+        unsigned int l_errorcount;
+        int read_fd;
+        char *fdname;
+        char *arg;
+        char *argv;
+        char *path;
+        int argc;
+}details;
+
 #endif
