98a3dc109271c76ff7c211b23a5db32ba468f58f
Moniaar <www.omniaahmed1562001@gmail.com>
2023-10-11 02:51:10 -0700

    debugging sesion 1

---
+++ sloop.c
@@ -0,0 +1,112 @@
+#include "shell.h"
+
+/**
+ * sloop - the shell main loop
+ *
+ * @n: a variable from struct details
+ *
+ * @av: argument vector
+ *
+ * Return: 0 Upon success, -1 upon error
+ */
+
+int sloop(details *n, char **av)
+{
+
+        ssize_t k;
+        int builtin;
+
+        k = 0;
+
+        builtin = 0;
+
+        while (k != -1 && builtin != -2)
+        {
+                clear_(n);
+                if (_interactive(n))
+                        display_prompt;
+                k = userinput(n);
+                if (r != -1)
+                {
+                        set_details(n, av);
+                        builtin = find_(n);
+                        if (builtin == -1)
+				find_command_in_path(n);
+                }
+                else if (_interactive(n))
+                        _putchar(10);
+
+        }
+        return (builtin);
+}
+
+/**
+ * find_ - a function to search for a built in command
+ *
+ * @n: paramater from details struct type
+ *
+ * Return: -1 if not found, 0 if built-in executed successfully,
+ * 1 if found but not successful. -2 for built-in signal only exit()
+ */
+
+int find_(details *n)
+{
+
+        int k, builtin;
+
+	builtin = -1;
+
+        k = 0;
+
+        built_t table[] = {
+                {"exit", _myexit},
+                {"cd", _newcd},
+                {"env", _env},
+                {NULL, NULL}
+        };
+
+        for (k = 0 ; table[k].flag ; k++)
+                if (_strcomp(n->argv[0], table[k].flag) == 0)
+                {
+                        n->l_errorcount++;
+                        builtin = table[k].fu(n);
+                        break;
+                }
+        return (builtin);
+}
+
+
+
+/**
+ * strcut built - contains a builtin string and some related functions
+ *
+ * @flag: pointer to the built in command flag
+ *
+ * @fu: common function
+ */
+
+struct built
+{
+        char *flag;
+        int(*fu)(*details);
+}built_t;
+
+
+/**
+ * struct common_arg - contains arguments to be passed into a function
+ *
+ * @l_errorcount: error counting
+ *
+ * @read_fd: the fd to be read from the input line
+ */
+
+struct common_arg
+{
+        unsigned int l_errorcount;
+        int read_fd;
+        char *fdname;
+        char *arg;
+        char *argv;
+        char *path;
+        int argc;
+}details;
