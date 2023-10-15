72c58316a8bae5dc796a0b3e732e0dfc5f52e7ba
Moniaar <www.omniaahmed1562001@gmail.com>
2023-10-15 12:11:56 -0700

    the scanner header file

---
+++ scanner.h
@@ -0,0 +1,17 @@
+#ifndef SCANNER_H
+#define SCANNER_H
+
+struct tokens
+{
+	struct source *s;       /* source of input */
+	int len_of_text;            /* length of token text */
+	char *token_text;               /* token text */
+};
+
+/* the special EOF token, which indicates the end of input */
+extern struct tokens end_of_tok;
+
+struct tokens *tokenizing(struct source *s);
+void token_freed(struct tokens *t);
+
+#endif
