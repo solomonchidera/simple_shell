a0c3c58bae0f8d78e31ce89a79a3db21748cd705
SolomonChidera <solochidera10@gmail.com>
2023-10-17 17:56:06 +0100

    A random file please

--- source.c
+++ source.c
@@ -2,9 +2,15 @@
 #include "solomon.h"
 #include "source.h"
 
+/**
+ * return_last_char - Funtion to check for the last character
+ *
+ * @s: variable to use.
+ * Return: nothing.
+*/
 void return_last_char(struct source *s)
 {
-	if(s->cur_pos < 0)
+	if (s->cur_pos < 0)
 	{
 		return;
 	}
@@ -12,18 +18,25 @@ void return_last_char(struct source *s)
 	s->cur_pos--;
 }
 
-
+/**
+ * Nextchar - Fuction to check next character
+ *
+ * @s: a struct member
+ * Return: s to the current position.
+ */
 char Nextchar(struct source *s)
 {
 	char char1;
-	if(!s || !s->bu)
+
+	if (!s || !s->bu)
 	{
 		errno = ENODATA;
-		return CHARERROR;
+		return (CHARERROR);
 	}
 
 	char1 = 0;
-	if(s->cur_pos == INIT_SRC_POS)
+
+	if (s->cur_pos == INIT_SRC_POS)
 	{
 		s->cur_pos  = -1;
 	}
@@ -32,53 +45,64 @@ char Nextchar(struct source *s)
 		char1 = s->bu[s->cur_pos];
 	}
 
-	if(++s->cur_pos >= s->busize)
+	if (++s->cur_pos >= s->busize)
 	{
 		s->cur_pos = s->busize;
-		return EOF;
+		return (EOF);
 	}
 
-	return s->bu[s->cur_pos];
+	return (s->bu[s->cur_pos]);
 }
 
 
+/**
+ * char_sneekpeek - sneek and peek the next character
+ * @s: struct member.
+ *
+ * Return: pointer to the next node.
+ */
 char char_sneekpeek(struct source *s)
 {
-	if(!s || !s->bu)
+	if (!s || !s->bu)
 	{
 		errno = ENODATA;
-		return CHARERROR;
+		return (CHARERROR);
 	}
 
 	long pos = s->cur_pos;
 
-	if(pos == INIT_SRC_POS)
+	if (pos == INIT_SRC_POS)
 	{
 		pos++;
 	}
 	pos++;
 
-	if(pos >= s->busize)
+	if (pos >= s->busize)
 	{
-		return EOF;
+		return (EOF);
 	}
 
-	return s->bu[pos];
+	return (s->bu[pos]);
 }
 
+/**
+ * skip_whitespace - Function that will skip the whitespace character.
+ * @s: struct member.
+ *
+ * Return: nothing.
+ */
 
 void skip_whitespace(struct source *s)
 {
 	char c;
 
-	if(!s || !s->bu)
+	if (!s || !s->bu)
 	{
 		return;
 	}
 
-	while(((c = char_sneekpeek(s)) != EOF) && (c == ' ' || c == '\t'))
+	while (((c = char_sneekpeek(s)) != EOF) && (c == ' ' || c == '\t'))
 	{
 		Nextchar(s);
 	}
 }
-
