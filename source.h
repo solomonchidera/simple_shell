1b3b4581544d48376d2a3d84ca3af66afbc139fe
SolomonChidera <solochidera10@gmail.com>
2023-10-17 17:47:00 +0100

    Header file for source.c file

--- source.h
+++ source.h
@@ -6,15 +6,23 @@
 
 #define INIT_SRC_POS    (-2)
 
+/**
+ * struct source - Just a gaddamn struct.
+ * @bu: the input text
+ * @busize: size of the input text
+ * @cur_pos: absolute char position in source
+ */
+
 struct source
 {
-    char *bu;       /* the input text */
-    long busize;       /* size of the input text */
-    long  cur_pos;       /* absolute char position in source */
+	char *bu;
+	long busize;
+	long  cur_pos;
 };
 
 char Nextchar(struct source *s);
 void return_last_char(struct source *s);
 char char_sneekpeek(struct source *s);
 void skip_whitespace(struct source *s);
+
 #endif
