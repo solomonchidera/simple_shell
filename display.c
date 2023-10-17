9113280203bb11ecec00b77912d49ed9ca3a052c
Moniaar <www.omniaahmed1562001@gmail.com>
2023-10-17 10:04:08 -0700

    the file that will display the prompt

--- display.c
+++ display.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include "solomon.h"
 
+/**
+ * display_prompt1 - a function to display our cute prompt for you
+ *
+ * Return: Nothing
+ */
+
 void display_prompt1(void)
 {
-        _puts(")--> ");
+	_puts(")--> ");
 }
+
+/**
+ * display_prompt2 - a function to display > character for you
+ *
+ * Return: Nothing
+ */
 
 void display_prompt2(void)
 {
-        _puts(">");
+	_puts(">");
 }
