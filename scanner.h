81d3aadb7d8b18d436936e270586b8a18467fe31
SolomonChidera <solochidera10@gmail.com>
2023-10-17 17:45:05 +0100

    Scanner file

--- scanner.h
+++ scanner.h
@@ -1,11 +1,33 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+/**
+ * struct source - Just a gaddamn struct.
+ * @bu: the input text
+ * @busize: size of the input text
+ * @cur_pos: absolute char position in source
+ */
+
+struct source
+{
+	char *bu;       /* the input text */
+	long busize;       /* size of the input text */
+	long  cur_pos;       /* absolute char position in source */
+};
+
+/**
+ * struct tokens - structure for tokenization.
+ * @s: just a random variable.
+ * @len_of_text: length of token text
+ * @token_text: token text.
+ */
+
 struct tokens
 {
-	struct source *s;       /* source of input */
-	int len_of_text;            /* length of token text */
-	char *token_text;               /* token text */
+	struct source *s;
+
+	int len_of_text;
+	char *token_text;
 };
 
 /* the special EOF token, which indicates the end of input */
