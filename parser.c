c7ae1e5b8975ef6228ed1247e37418a08c31c8bc
Moniaar <www.omniaahmed1562001@gmail.com>
2023-10-17 10:08:48 -0700

    the parser handling function is hiding here

--- parser.c
+++ parser.c
@@ -1,48 +1,56 @@
-#include <unistd.h>
 #include "solomon.h"
 #include "node.h"
 #include "parser.h"
 #include "scanner.h"
 #include "source.h"
 
+/**
+ * parse_simple_cmd - a function to parse simple commands
+ *
+ * @tk: a struct type from tokens user-defined structre
+ *
+ * Return: struct nodes type to point to the commands
+ */
+
 struct nodes *parse_simple_cmd(struct tokens *tk)
 {
-	if(!tk)
+	if (!tk)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	struct nodes *command = Newnode(NODE_CMD);
-	if(!command)
+
+	if (!command)
 	{
 		token_freed(tk);
-		return NULL;
+		return (NULL);
 	}
 
 	struct source *s = tk->s;
 
-	do
-	{
-		if(tk->token_text[0] == '\n')
+	do {
+		if (tk->token_text[0] == '\n')
 		{
 			token_freed(tk);
 			break;
 		}
 
 		struct nodes *Word = Newnode(NODE_V);
-		if(!Word)
+
+		if (!Word)
 		{
 			free_nodetree(command);
 			token_freed(tk);
-			return NULL;
+			return (NULL);
 		}
 		set_node_string(Word, tk->token_text);
 		add_childnode(command, Word);
 
 		token_freed(tk);
 
-	} while((tk = tokenizing(s)) != &end_of_tok);
+	} while ((tk = tokenizing(s)) != &end_of_tok);
 
-	return command;
+	return (command);
 }
 
