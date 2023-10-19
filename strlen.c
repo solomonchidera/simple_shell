40ac2d658658ef044cb567f6d3b47beb86e7e2f7
SolomonChidera <solochidera10@gmail.com>
2023-10-19 01:26:51 +0100

    Add: strlen keep count of a string

---
+++ strlen.c
@@ -0,0 +1,20 @@
+#include "moniaar.h"
+
+/**
+ * _strlen - A function to return the length of a string
+ *
+ * @s: input character
+ *
+ * Return: int length of a string
+ */
+
+int _strlen(char *s)
+{
+	int cnu;
+
+	for (cnu = 0 ; *s != '\0' ; s++)
+	{
+		++cnu;
+	}
+	return (cnu);
+}
