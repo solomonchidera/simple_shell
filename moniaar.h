c70e5618fc5bf78a51a3c0c1faabb0018628b843
SolomonChidera <solochidera10@gmail.com>
2023-10-19 01:12:51 +0100

    Add: Header file with the needed prototypes and library

---
+++ moniaar.h
@@ -0,0 +1,40 @@
+#ifndef MONIAAR_H
+#define MONIAAR_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define TOK_DELIM " \t\r\n\a\""
+extern char **environ;
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+#define my_sizeof(type) (char *)(&type + 1) - (char *)(&type)
+int main(void);
+
+/* main.c */
+void shell_is_interactive(void);
+void shell_no_interactive(void);
+
+/* shell_interactive.c */
+char *read_line(void);
+char **split_line(char *line);
+int execute_args(char **args);
+
+/* execute_args */
+int new_process(char **args);
+
+/* shell_no_interactive */
+char *read_stream(void);
+
+/*---Builtin func---*/
+int cd(char **args);
+int own_exit(char **args);
+int env(char **args);
+int assist(char **args);
+
+#endif
