2e0e12290eff3b7dc237207dfd3eaf568de47519
SolomonChidera <solochidera10@gmail.com>
2023-10-19 01:46:20 +0100

    Custom function that act like puts [funny right?]

---
+++ puts.c
@@ -0,0 +1,19 @@
+#include "moniaar.h"
+
+/**
+ * _puts - function that prints a string to stdout
+ *
+ * @str: char pointer variable
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
