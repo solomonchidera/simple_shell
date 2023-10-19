39e2cc2a8602ee060b3a6ef58efafcc4f36071b0
SolomonChidera <solochidera10@gmail.com>
2023-10-19 01:04:45 +0100

    Better ones are coming

--- parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "scanner.h"    /* struct token_s */
-#include "source.h"     /* struct source_s */
-#include "solomon.h"
-struct nodes *parse_simple_cmd(struct tokens *tk);
-#endif
